7429f33afd5c0889acbb4bd701b6658af56440fc
smitharaoas <smitharao90@gmail.com>
2025-05-07 18:15:37 +0530

    mouse operations

---
+++ Basics/test_mouseOperations.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+from Basics.conftest import browser_driver
+
+
+def tes_mouseoperations(browser_driver):
+    driver = browser_driver()
+    driver.get("https://rahulshettyacademy.com/AutomationPractice/#/")
+    action = ActionChains(driver)
+    action.move_to_element(driver.find_element(By.CSS_SELECTOR, "#mousehover")).perform()
+    action.context_click(driver.find_element(By.LINK_TEXT, "Top")).perform()
+    action.context_click(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
+
