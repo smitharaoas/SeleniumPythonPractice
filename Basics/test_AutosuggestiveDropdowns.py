c4fe7a8ad2ef3d7a45c8d23c28ae47502540cbe1
smitharaoas <smitharao90@gmail.com>
2025-05-07 16:54:18 +0530

    Updated framework for AUtosuggestive dropdowns

---
+++ Basics/test_AutosuggestiveDropdowns.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_autosuggest(browser_driver):
+    driver = browser_driver
+    driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+    driver.find_element(By.CSS_SELECTOR, "input[id='autosuggest']").send_keys("ind")
+    time.sleep(2)
+    countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
+    for country in countries:
+        if country.text == "India":
+            country.click()
+            break
+    countrySelected = driver.find_element(By.CSS_SELECTOR, "#autosuggest")
+    assert countrySelected.get_attribute("value") == "India"
